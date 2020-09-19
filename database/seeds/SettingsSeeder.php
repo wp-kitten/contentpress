43c98e805011e81dc9801d09b2c2a52cc3511706
wp-kitten <wp.kytten@gmail.com>
2020-09-20 01:11:38 +0300

    Refactor seed code
    Closes #4

--- database/seeds/SettingsSeeder.php
+++ database/seeds/SettingsSeeder.php
@@ -14,73 +14,33 @@ class SettingsSeeder extends Seeder
      */
     public function run()
     {
-        //#! Settings > General
-        Settings::create( [
-            'name' => 'default_language',
-            'value' => 'en',
-        ] );
-        Settings::create( [
-            'name' => 'default_post_status',
-            'value' => 'draft',
-        ] );
-        Settings::create( [
-            'name' => 'user_registration_open',
-            'value' => 0,
-        ] );
-        Settings::create( [
-            'name' => 'registration_verify_email',
-            'value' => 0,
-        ] );
-        Settings::create( [
-            'name' => 'allow_user_reset_password',
-            'value' => 1,
-        ] );
-        Settings::create( [
-            'name' => 'default_user_role',
-            'value' => Role::where( 'name', Role::ROLE_MEMBER )->first()->id,
-        ] );
-        Settings::create( [
-            'name' => 'default_comment_status',
-            'value' => CommentStatuses::where( 'name', 'pending' )->first()->id,
-        ] );
-        Settings::create( [
-            'name' => 'anyone_can_comment',
-            'value' => false,
-        ] );
-        Settings::create( [
-            'name' => 'site_title',
-            'value' => 'ContentPress',
-        ] );
-        Settings::create( [
-            'name' => 'site_description',
-            'value' => 'WordPress-like application using the Laravel Framework.',
-        ] );
-        Settings::create( [
-            'name' => 'blog_title',
-            'value' => 'Blog',
-        ] );
-        Settings::create( [
-            'name' => 'date_format',
-            'value' => 'M j, Y',
-        ] );
-        Settings::create( [
-            'name' => 'time_format',
-            'value' => 'G:i',
-        ] );
+        $settings = [
+            //#! Settings > General
+            'default_language' => 'en',
+            'default_post_status' => 'draft',
+            'user_registration_open' => 0,
+            'registration_verify_email' => 0,
+            'allow_user_reset_password' => 1,
+            'default_user_role' => Role::where( 'name', Role::ROLE_MEMBER )->first()->id,
+            'default_comment_status' => CommentStatuses::where( 'name', 'pending' )->first()->id,
+            'anyone_can_comment' => false,
+            'site_title' => 'ContentPress',
+            'site_description' => 'WordPress-like application using the Laravel Framework.',
+            'blog_title' => 'Blog',
+            'date_format' => 'M j, Y',
+            'time_format' => 'G:i',
+            //#! Settings > Reading
+            'posts_per_page' => 10,
+            'comments_per_page' => 10,
+            //#! What the frontpage displays: the blog page (blog) or a specific page (page)
+            'show_on_front' => 'blog',
+        ];
 
-        //#! Settings > Reading
-        Settings::create( [
-            'name' => 'posts_per_page',
-            'value' => 10,
-        ] );
-        Settings::create( [
-            'name' => 'comments_per_page',
-            'value' => 10,
-        ] );
-        //#! What the frontpage displays: the blog page (blog) or a specific page (page)
-        Settings::create( [
-            'name' => 'show_on_front',
-            'value' => 'blog', // blog or page
-        ] );
+        foreach ( $settings as $name => $value ) {
+            Settings::create( [
+                'name' => $name,
+                'value' => $value,
+            ] );
+        }
     }
 }
