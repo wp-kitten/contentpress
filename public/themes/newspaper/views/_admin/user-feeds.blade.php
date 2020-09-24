468d5371a3682c432d4ea94aff2192e72eadfe02
wp-kitten <wp.kytten@gmail.com>
2020-09-25 02:33:39 +0300

    Adds pagination to user feeds

--- public/themes/newspaper/views/_admin/user-feeds.blade.php
+++ public/themes/newspaper/views/_admin/user-feeds.blade.php
@@ -122,6 +122,8 @@
                                 @endforelse
                             </ul>
                         </div>
+                        
+                        {!! $feeds->render() !!}
                     @else
                         <div class="bs-component">
                             <div class="alert alert-info">
