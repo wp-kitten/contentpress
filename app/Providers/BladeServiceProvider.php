be5186dec9b9996dd42404433acf8270965fb2b7
wp-kitten <wp.kytten@gmail.com>
2020-10-15 16:42:45 +0300

    Removed Blade directives

--- app/Providers/BladeServiceProvider.php
+++ app/Providers/BladeServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Helpers\ScriptsManager;
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class BladeServiceProvider extends ServiceProvider
@@ -25,24 +23,6 @@ class BladeServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Blade::directive( 'cp_head', function () {
-            ob_start();
-            do_action( 'contentpress/site/head' );
-            ScriptsManager::printStylesheets();
-            ScriptsManager::printLocalizedScripts();
-            ScriptsManager::printHeadScripts();
-            $html = ob_get_contents();
-            ob_end_clean();
-            return $html;
-        } );
-
-        Blade::directive( 'cp_footer', function () {
-            ob_start();
-            do_action( 'contentpress/site/footer' );
-            ScriptsManager::printFooterScripts();
-            $html = ob_get_contents();
-            ob_end_clean();
-            return $html;
-        } );
+        //...
     }
 }
