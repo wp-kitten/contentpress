c2951149b04a25ec9356cfac261f5ae3ee7a0fd8
wp-kitten <wp.kytten@gmail.com>
2020-12-24 14:28:36 +0200

    Version bump

--- app/Helpers/inc/_globals.php
+++ app/Helpers/inc/_globals.php
@@ -6,7 +6,7 @@ use App\Models\Post;
  * The current version of the application
  * @var float
  */
-define( 'CONTENTPRESS_VERSION', '0.12' );
+define( 'CONTENTPRESS_VERSION', '0.13' );
 /**
  * Holds the url to the ContentPress website
  * @var string
