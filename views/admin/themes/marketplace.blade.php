cb301af9bdf552815963fe3aa7e81abdf0c8f37c
wp-kitten <wp.kytten@gmail.com>
2020-12-31 20:45:28 +0200

    Code format

--- views/admin/themes/marketplace.blade.php
+++ views/admin/themes/marketplace.blade.php
@@ -26,53 +26,53 @@
     @if(vp_current_user_can('list_themes'))
         <div class="row themes-list marketplace">
             @if($themes)
-            @foreach($themes as $themeDirName => $themeInfo)
-                @php
-                    $isActive = ($currentTheme->get('name') == $themeDirName);
-                @endphp
+                @foreach($themes as $themeDirName => $themeInfo)
+                    @php
+                        $isActive = ($currentTheme->get('name') == $themeDirName);
+                    @endphp
 
-                <div class="col-sm-12 col-md-3">
-                    <div class="card @if($isActive) active @endif">
-                        <div class="card-body">
-                            @if($themeInfo['thumbnail'])
-                                <a href="{{$themeInfo['page_url']}}"
-                                   target="_blank"
-                                   title="{{__('a.Visit theme site')}}"
-                                   class="theme-title @if($isActive) theme-active text-success @else text-dark @endif">
-                                    <img src="{{url(path_combine(VALPRESS_MARKETPLACE_URL, 'themes', $themeDirName, $themeInfo['thumbnail']))}}"
-                                         alt="{{$themeInfo['display_name']}}"
-                                         class="img-thumbnail"/>
-                                </a>
-                            @endif
-                        </div>
-                        <div class="card-footer">
-                            <div class="theme-actions d-flex">
-                                <h4 class="theme-title title-sm mr-auto">{{$themeInfo['display_name']}}</h4>
-                                <div>
-                                    @if(vp_current_user_can('switch_themes'))
-                                        @if(! $themesManager->exists($themeDirName))
-                                            <a href="#"
-                                               onclick="event.preventDefault(); document.getElementById('form-theme-install-{{$themeDirName}}').submit();"
-                                               class="text-light btn btn-primary btn-sm">
-                                                {{__('a.Install')}}
-                                            </a>
-                                            <form id="form-theme-install-{{$themeDirName}}"
-                                                  action="{{route('admin.themes.marketplace.install', [
+                    <div class="col-sm-12 col-md-3">
+                        <div class="card @if($isActive) active @endif">
+                            <div class="card-body">
+                                @if($themeInfo['thumbnail'])
+                                    <a href="{{$themeInfo['page_url']}}"
+                                       target="_blank"
+                                       title="{{__('a.Visit theme site')}}"
+                                       class="theme-title @if($isActive) theme-active text-success @else text-dark @endif">
+                                        <img src="{{url(path_combine(VALPRESS_MARKETPLACE_URL, 'themes', $themeDirName, $themeInfo['thumbnail']))}}"
+                                             alt="{{$themeInfo['display_name']}}"
+                                             class="img-thumbnail"/>
+                                    </a>
+                                @endif
+                            </div>
+                            <div class="card-footer">
+                                <div class="theme-actions d-flex">
+                                    <h4 class="theme-title title-sm mr-auto">{{$themeInfo['display_name']}}</h4>
+                                    <div>
+                                        @if(vp_current_user_can('switch_themes'))
+                                            @if(! $themesManager->exists($themeDirName))
+                                                <a href="#"
+                                                   onclick="event.preventDefault(); document.getElementById('form-theme-install-{{$themeDirName}}').submit();"
+                                                   class="text-light btn btn-primary btn-sm">
+                                                    {{__('a.Install')}}
+                                                </a>
+                                                <form id="form-theme-install-{{$themeDirName}}"
+                                                      action="{{route('admin.themes.marketplace.install', [
                                                             'theme_dir_name' => $themeDirName,
                                                             'version' => $themeInfo['version']
                                                         ])}}"
-                                                  method="post"
-                                                  class="hidden">
-                                                @csrf
-                                            </form>
+                                                      method="post"
+                                                      class="hidden">
+                                                    @csrf
+                                                </form>
+                                            @endif
                                         @endif
-                                    @endif
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
             @else
                 <div class="col-sm-12">
                     <div class="alert alert-info">
